004ff86297ae17b1a206cd274378b55edcf66528
hypernova1 <hypemova@gmail.com>
2024-07-01 10:53:35 +0900

    test: 상품 수량 동시성 테스트 변경

--- src/test/java/kr/co/_29cm/homework/product/application/ProductServiceTest.java
+++ src/test/java/kr/co/_29cm/homework/product/application/ProductServiceTest.java
@@ -1,6 +1,7 @@
 package kr.co._29cm.homework.product.application;
 
 import kr.co._29cm.homework.common.lock.LockManager;
+import kr.co._29cm.homework.common.lock.MemoryLockManager;
 import kr.co._29cm.homework.product.domain.Product;
 import kr.co._29cm.homework.product.domain.ProductRepository;
 import kr.co._29cm.homework.product.domain.SoldOutException;
@@ -13,8 +14,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,46 +26,45 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
+
+    private final LockManager lockManager = new MemoryLockManager();
 
     @Mock
     private ProductRepository productRepository;
 
-    @Autowired
-    private LockManager lockManager;
-
     @InjectMocks
     private ProductService productService;
 
+    private final static int STOCK = 10;
+    private final String PRODUCT_NO = "100000";
+
     @BeforeEach
-    public void setUp() {
-        productService = new ProductService(productRepository, lockManager);
+    void setUp() {
+        this.productService = new ProductService(productRepository, lockManager);
+        Product testProduct = new Product(PRODUCT_NO, "테스트 상품", 10000, new Stock(STOCK));
+        when(productRepository.findByProductNoIn(List.of(testProduct.getProductNo())))
+                .thenReturn(List.of(testProduct));
     }
 
     @Test
     @DisplayName("멀티 스레드 환경에서 SoldOutException이 정확하게 동작하는지 확인")
     void test_multi_thread_test() throws InterruptedException {
+        //given
         int NUMBER_OR_THREADS = 500;
         int LOOP_COUNT = 100;
 
-        int stock = 50;
-
-        Product product = new Product("10000", "테스트 상품", 10_000, new Stock(stock));
         List<ProductQuantityDto> productQuantityDtoList = new ArrayList<>();
-        ProductQuantityDto productQuantityDto = new ProductQuantityDto(product.getProductNo(), 1);
+        ProductQuantityDto productQuantityDto = new ProductQuantityDto(PRODUCT_NO, 1);
         productQuantityDtoList.add(productQuantityDto);
-
-        List<String> productNoList = productQuantityDtoList.stream().map(ProductQuantityDto::productNo).toList();
-        List<Product> products = List.of(product);
-
-        when(productRepository.findByProductNoIn(productNoList)).thenReturn(products);
 
         ExecutorService executorService = Executors.newFixedThreadPool(NUMBER_OR_THREADS);
         CountDownLatch latch = new CountDownLatch(NUMBER_OR_THREADS);
 
         AtomicInteger numberOfException = new AtomicInteger();
+
+        //when
         for (int i = 0; i < NUMBER_OR_THREADS; i++) {
             executorService.execute(() -> {
                 for (int j = 0; j < LOOP_COUNT; j++) {
@@ -83,7 +81,8 @@ class ProductServiceTest {
         latch.await();
         executorService.shutdown();
 
-        assertThat(NUMBER_OR_THREADS * LOOP_COUNT - stock).isEqualTo(numberOfException.get());
+        //then
+        assertThat(NUMBER_OR_THREADS * LOOP_COUNT - STOCK).isEqualTo(numberOfException.get());
     }
 
 }
